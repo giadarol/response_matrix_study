2124b82acde2a5d3eecef7b3abd9e6a5abe1d07c
giadarol <giovanni.iadarola@cern.ch>
2020-02-02 13:01:18 +0100

    Working on footprint

---
+++ 004_instability_simulation/001_compute_footprint.py
@@ -0,0 +1,110 @@
+import os
+import time
+
+import numpy as np
+
+import PyECLOUD.myfilemanager as mfm
+from PyPARIS_sim_class import Simulation as sim_mod
+import PyPARIS.util as pu
+
+# Import response_matrix
+import sys
+sys.path.append('../')
+import response_matrix.response_matrix as rm
+
+
+# start-settings-section
+recenter_all_slices = True # Cancels initial kick from input
+
+ecloud_strength_scale = 1.
+
+sim_param_file = '../reference_simulation/Simulation_parameters.py'
+sim_param_amend_files = ['../Simulation_parameters_amend.py',
+                    'Simulation_parameters_amend_for_matrixsim.py']
+
+include_non_linear_map = True
+field_map_file = '../003_generate_field_map/field_map.mat'
+# end-settings-section
+
+
+# Instantiate simulation
+sim_content = sim_mod.Simulation(param_file=sim_param_file)
+
+# Here sim_content.pp can be edited (directly and through files)
+for ff in sim_param_amend_files:
+    sim_content.pp.update(param_file=ff)
+
+# Remove RF and switch off damper
+
+
+# Disable real e-clouds
+sim_content.pp.enable_arc_dip = False
+sim_content.pp.enable_arc_quad = False
+
+# Add ring of CPU information
+ring_cpu = pu.get_serial_CPUring(sim_content,
+        init_sim_objects_auto=False)
+assert(sim_content.ring_of_CPUs.I_am_the_master)
+
+# Initialize machine elements
+sim_content.init_all()
+
+# Initialize beam, slicer, monitors, multijob mode
+if os.path.exists('simulation_status.sta'):
+    os.remove('simulation_status.sta')
+sim_content.init_master(generate_bunch=False,
+        prepare_monitors=False)
+
+# Get bunch, slicer, machine and monitors
+slicer = sim_content.slicer
+machine = sim_content.machine
+
+# Generate bunch for footprint
+pp = sim_content.pp
+bunch = sim_content.machine.generate_6D_Gaussian_bunch_matched(
+    n_macroparticles=pp.n_macroparticles_for_footprint_track,
+    intensity=pp.intensity,
+    epsn_x=pp.epsn_x,
+    epsn_y=pp.epsn_y,
+    sigma_z=pp.sigma_z,
+)
+
+# Slice the bunch
+slices = bunch.extract_slices(slicer)
+
+# Recenter all slices
+if recenter_all_slices and sim_content.SimSt.first_run:
+    slices = bunch.get_slices(slicer)
+    for ii in range(slices.n_slices):
+        ix = slices.particle_indices_of_slice(ii)
+        if len(ix) > 0:
+            bunch.x[ix] -= np.mean(bunch.x[ix])
+            bunch.xp[ix] -= np.mean(bunch.xp[ix])
+
+# Introduce non-linear field map
+if include_non_linear_map:
+
+    obfmap = mfm.myloadmat_to_obj(field_map_file)
+    from PyECLOUD.Transverse_Efield_map_for_frozen_cloud import Transverse_Efield_map
+    fmap = Transverse_Efield_map(
+        xg=obfmap.xg,
+        yg=obfmap.yg,
+        Ex=obfmap.Ex_L_map,
+        Ey=obfmap.Ey_L_map,
+        L_interaction=1./sim_content.n_segments*ecloud_strength_scale,
+        slicer=slicer,
+        flag_clean_slices=False,
+        wrt_slice_centroid=True,
+        x_beam_offset=0.,
+        y_beam_offset=0.,
+        slice_by_slice_mode=False)
+    machine.install_after_each_transverse_segment(fmap)
+
+
+# Simulate
+slice_x_list = []
+for i_turn in range(sim_content.N_turns):
+    print('%s Turn %d' % (time.strftime("%d/%m/%Y %H:%M:%S", time.localtime()), i_turn))
+
+    machine.track(bunch)
+
