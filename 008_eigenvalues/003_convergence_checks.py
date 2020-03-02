b4cbec612e3ae3742141063b0e65abfbaadb3465
giadarol <giovanni.iadarola@cern.ch>
2020-03-02 16:12:37 +0100

    Forgotten a file

---
+++ 008_eigenvalues/003_convergence_checks.py
@@ -0,0 +1,61 @@
+import pickle
+
+import numpy as np
+from scipy.constants import c as clight
+
+pkl_fname = 'mode_coupling_matrix.pkl'
+
+l_min = -3
+l_max = 3
+m_max = 3
+
+with open(pkl_fname, 'rb') as fid:
+    MM_orig = pickle.load(fid)
+
+MM_obj = MM_orig.get_sub_matrix(l_min, l_max, m_max)
+
+omega0 = 2*np.pi*clight/27e3 # Revolution angular frquency
+omega_s = 4.9e-3*omega0
+
+# Mode coupling test
+strength_scan = np.arange(0, 3.4, 0.02)
+Omega_mat = MM_obj.compute_mode_complex_freq(omega_s, rescale_by=strength_scan)
+
+import matplotlib.pyplot as plt
+plt.close('all')
+
+mask_unstable = np.imag(Omega_mat) > 1e-1
+Omega_mat_unstable = Omega_mat.copy()
+Omega_mat_unstable[~mask_unstable] = np.nan
+
+i_mode = -1
+mask_mode = np.abs(np.real(Omega_mat)/omega_s - i_mode)<0.5
+plt.plot(strength_scan, np.imag(Omega_mat), '.b')
+plt.plot(strength_scan, np.imag(Omega_mat), '.b')
+Omega_mat_mode = Omega_mat.copy()
+Omega_mat_mode[~mask_mode] = np.nan
+
+plt.figure(200)
+plt.plot(strength_scan, np.real(Omega_mat)/omega_s, '.b')
+plt.plot(strength_scan, np.real(Omega_mat_unstable)/omega_s, '.r')
+
+
+plt.figure(201)
+plt.plot(strength_scan, np.imag(Omega_mat), '.b')
+plt.plot(strength_scan, np.imag(Omega_mat_mode), '.g')
+
+plt.figure(300)
+plt.plot(np.imag(Omega_mat).flatten(),
+        np.real(Omega_mat).flatten()/omega_s, '.b')
+
+plt.figure(400)
+for ii in range(len(strength_scan)):
+    plt.scatter(x=strength_scan[ii]+0*np.imag(Omega_mat[ii, :]),
+            y=np.imag(Omega_mat[ii, :]),
+            c = np.real(Omega_mat[ii, :])/omega_s,
+            vmin=-2, vmax=2, cmap=plt.cm.seismic)
+plt.colorbar()
+
+
+plt.show()
+
