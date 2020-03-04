4bf101dc52a68cb5c551bde05546e4003a86791f
giadarol <giovanni.iadarola@cern.ch>
2020-03-04 19:07:35 +0100

    Working on cleaning

--- 001_sin_response_scan/002_inspect_matrix.py
+++ 001_sin_response_scan/002_inspect_matrix.py
@@ -37,21 +37,23 @@ for ii in range(n_tail_cut):
 
 
 WW = np.dot(MM, np.dot(CC, np.dot(RR_inv, np.dot(FF.T, CC_tails))))
-
 WW_cleaned = np.zeros_like(WW)
+n_zero = 30
+for i_work in range(len(z_resp)):
+    if i_work < n_zero or i_work > len(z_resp) - n_zero:
+        continue
 
-i_work = 200-30
-resp_work = WW[:, i_work]
+    resp_work = WW[:, i_work]
 
-n_copy_max = 50
+    n_copy = min([len(z_resp)-i_work-2, i_work-2])
+    resp_symm = resp_work.copy()
+    resp_symm[i_work+2 : i_work+2 + n_copy] = resp_symm[i_work-n_copy-2:i_work-2][::-1]
+    resp_symm[i_work-2:i_work+2] = resp_symm[i_work+2]
+    resp_filt = savgol_filter(resp_symm, window_length=21, polyorder=5)
+    resp_out = resp_filt.copy()
+    resp_out[i_work:] = 0.
 
-n_copy = min([n_copy_max, len(z_resp)-i_work-2, i_work-2])
-resp_symm = resp_work.copy()
-resp_symm[i_work+2 : i_work+2 + n_copy] = resp_symm[i_work-n_copy-2:i_work-2][::-1]
-resp_symm[i_work-2:i_work+2] = resp_symm[i_work+2]
-resp_filt = savgol_filter(resp_symm, window_length=21, polyorder=5)
-resp_out = resp_filt.copy()
-resp_out[i_work:] = 0.
+    WW_cleaned[:, i_work] = resp_out
 
 plt.close('all')
 
@@ -62,14 +64,15 @@ ax1.plot(z_resp, resp_symm)
 ax1.plot(z_resp, resp_filt)
 ax1.plot(z_resp, resp_out)
 
+WW_plot = WW_cleaned
 
 fig30 = plt.figure(30)
 ax31 = fig30.add_subplot(111)
-ax31.matshow(np.abs(WW))
+ax31.matshow(np.abs(WW_plot))
 
 fig31 = plt.figure(31)
 ax311 = fig31.add_subplot(111)
-ax311.matshow(WW - np.diag(np.diag(WW)))
+ax311.matshow(WW_plot - np.diag(np.diag(WW_plot)))
 
 
 fig50 = plt.figure(50)
@@ -79,8 +82,8 @@ ax61 = fig60.add_subplot(111)
 import PyECLOUD.mystyle as ms
 for ii in list(range(0, 200, 1))[::-1]:
     colorcurr = ms.colorprog(ii, 200)
-    ax51.plot(z_resp, WW[:, ii], color=colorcurr)
-    ax61.plot(z_resp-z_resp[ii], WW[:, ii], '-', color=colorcurr)
+    ax51.plot(z_resp, WW_plot[:, ii], color=colorcurr)
+    ax61.plot(z_resp-z_resp[ii], WW_plot[:, ii], '-', color=colorcurr)
 
 fig70 = plt.figure(70)
 ax71 = fig70.add_subplot(111)
