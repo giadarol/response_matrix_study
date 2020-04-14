b1c6a5c70732170e45d7681d66fc428723d42c46
giadarol <giovanni.iadarola@cern.ch>
2020-04-14 11:14:40 +0200

    Status

--- 001_sin_response_scan/003_plot_a_single_one.py
+++ 001_sin_response_scan/003_plot_a_single_one.py
@@ -1,65 +1,90 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+
+from scipy.constants import e as qe
+
 import PyECLOUD.myfilemanager as mfm
 import PyECLOUD.mystyle as ms
 
-import numpy as np
-
 folder_sims = 'simulations'
+response_summary_file = './response_data_processed.mat'
+vmax_edens = 2.5e14
 
+i_plot_list = list(range(1,200))
+close_figures = True
 
-sin_ampl = 0
-cos_ampl = 1e-4
-n_osc_list = range(200)
+obsum = mfm.myloadmat_to_obj(response_summary_file)
 
-cos_ampl = 0
-sin_ampl = 1e-4
-n_osc_list = range(1,200)[5:6]
+plt.close('all')
+ms.mystyle_arial(fontsz=14, dist_tick_lab=5, traditional_look=False)
+for ii in i_plot_list:
 
-for n_osc in n_osc_list:
+    n_osc = obsum.n_osc_list[ii]
+    cos_ampl = obsum.cos_ampl_list[ii]
+    sin_ampl = obsum.sin_ampl_list[ii]
 
     current_sim_ident= f'n_{n_osc:.1f}_c{cos_ampl:.2e}_s{sin_ampl:.2e}'
     ob = mfm.myloadmat_to_obj(folder_sims + '/' + current_sim_ident + '/response.mat')
 
-    import matplotlib.pyplot as plt
-    plt.close('all')
-    ms.mystyle(fontsz=14, traditional_look=False)
-    fig1 = plt.figure(1)
-    ax1 = fig1.add_subplot(3,1,1)
-    ax2 = fig1.add_subplot(3,1,2, sharex=ax1)
-    ax3 = fig1.add_subplot(3,1,3, sharex=ax1)
+    #fig1 = plt.figure(100+ii)
+    #ax1 = fig1.add_subplot(3,1,1)
+    #ax2 = fig1.add_subplot(3,1,2, sharex=ax1)
+    #ax3 = fig1.add_subplot(3,1,3, sharex=ax1)
 
-    #ax1.plot(ob.z_slices, ob.int_slices)
-    ax2.plot(ob.z_slices, ob.x_slices)
-    ax3.plot(ob.z_slices, ob.dpx_slices_all_clouds)
+    ##ax1.plot(ob.z_slices, ob.int_slices)
+    #ax2.plot(ob.z_slices, ob.x_slices)
+    #ax3.plot(ob.z_slices, obsum.dpx_mat[ii, :])
 
-    for ax in [ax1, ax2, ax3]:
-        ax.grid(True)
+    #for ax in [ax1, ax2, ax3]:
+    #    ax.grid(True)
 
-    fig2 = plt.figure(2)
-    ax21 = fig2.add_subplot(2,1,1)
-    ax22 = fig2.add_subplot(2,1,2, sharex=ax21)
+    fig2 = plt.figure(200+ii)
+    #ax21 = fig2.add_subplot(2,1,1)
+    #ax22 = fig2.add_subplot(2,1,2, sharex=ax21)
+    pos_col1 = 0.15
+    width_col1 = .6
+    pos_row1=.55
+    height_row1 = .35
+    pos_row2 = .12
+    height_row2 = .35
+    ax21 = fig2.add_axes((pos_col1, pos_row1, width_col1, height_row1))
+    axcb = fig2.add_axes((pos_col1+width_col1+0.05, pos_row1,
+                    width_col1*0.07, height_row1))
+    ax22 = fig2.add_axes((pos_col1, pos_row2, width_col1, height_row2),
+            sharex=ax21)
 
-    ax21.pcolormesh(ob.z_slices, 1e3*ob.xg, ob.rho_cut.T)
-    ax21.plot(ob.z_slices, 1e3*ob.x_slices, 'k', lw=2)
-    ax22.plot(ob.z_slices, 1e6*ob.dpx_slices_all_clouds)
-    ax22.set_ylim(1e6*np.nanmax(np.abs(ob.dpx_slices_all_clouds))*np.array([-1.1, 1.1]))
+    mpbl = ax21.pcolormesh(1e2*ob.z_slices, 1e3*ob.xg, -(1/qe)*ob.rho_cut.T,
+            vmin=0, vmax=vmax_edens)
+    plt.colorbar(mpbl, cax=axcb)
+    ax21.plot(1e2*ob.z_slices, 1e3*ob.x_slices, 'k', lw=2)
+    ax22.plot(1e2*ob.z_slices, 1e6*obsum.dpx_mat[ii, :], lw=2)
     ax21.set_ylim(-2.5, 2.5)
+    ax22.set_ylim(1e6*np.nanmax(np.abs(obsum.dpx_mat[ii, :]))*np.array([-1.1, 1.1]))
+    ax22.set_ylim(0.2*np.array([-1.1, 1.1]))
 
     ax21.set_ylabel('x [mm]')
+    ax21.set_xlim(-30, 30)
     ax21.set_ylim(-2.5, 2.5)
-    ax22.set_ylabel('Dpx [urad]')
-    ax22.set_xlabel('z [m]')
+    ax22.set_ylabel(r'$\Delta$p$_{x}$ [$\mu$rad]')
+    ax22.set_xlabel('z [cm]')
     ax22.grid(linestyle=':', alpha=.9)
+    axcb.set_ylabel(r'Charge density [e$^-$/m$^3$]')
+    for ax in [ax21, ax22]:
+        ax.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
 
     fig2.suptitle(f'n={n_osc} ' + {True: 'sine', False: 'cosine'}[sin_ampl>cos_ampl], fontsize=14)
     fig2.subplots_adjust(
         top=0.91,
-        bottom=0.115,
+        bottom=0.12,
         left=0.16,
         right=0.915,
         hspace=0.2,
         wspace=0.2)
-    
+
     fig2.savefig(current_sim_ident + '.png', dpi=200)
+    if close_figures:
+        plt.close(fig2)
 
 plt.show()
 
--- 002_compare_single_interaction/001_compare_against_trace.py
+++ 002_compare_single_interaction/001_compare_against_trace.py
@@ -1,10 +1,14 @@
 import os
 
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+from scipy.constants import e as qe
 
 from PyPARIS_sim_class import Simulation as sim_mod
 import PyPARIS.util as pu
 import PyECLOUD.myfilemanager as mfm
+import PyECLOUD.mystyle as ms
 
 # Import response_matrix
 import sys
@@ -13,7 +17,7 @@ import response_matrix.response_matrix as rm
 
 test_data_file = './refsim_turn302.mat'
 #n_terms_list = range(1, 201, 2)
-n_terms_list = [12]
+n_terms_list = [6, 8, 10]
 n_tail_cut = 10
 response_data_file = '../001_sin_response_scan/response_data_processed.mat'
 z_strength_file = '../001a_sin_response_scan_unperturbed/linear_strength.mat'
@@ -52,18 +56,38 @@ if os.path.exists('simulation_status.sta'):
 # Initialize beam
 sim_content.init_master()
 
-for n_terms_to_be_kept in n_terms_list:
-    # Get bunch and slicer
-    bunch = sim_content.bunch
-    slicer = sim_content.slicer
+# Get bunch and slicer
+bunch = sim_content.bunch
+slicer = sim_content.slicer
 
-    # Recenter all slices
-    slices = bunch.get_slices(slicer)
-    for ii in range(slices.n_slices):
-        ix = slices.particle_indices_of_slice(ii)
-        if len(ix) > 0:
-            bunch.x[ix] -= np.mean(bunch.x[ix])
-            bunch.xp[ix] -= np.mean(bunch.xp[ix])
+
+# Get simulation data
+obsim = mfm.myloadmat_to_obj(test_data_file)
+x_test = obsim.x_slices
+int_test = obsim.int_slices
+x_test[np.isnan(x_test)] = 0.
+xg = obsim.xg
+yg = obsim.yg
+
+
+plt.close('all')
+ms.mystyle_arial(fontsz=14, dist_tick_lab=5, traditional_look=False)
+
+figglob = plt.figure(1, figsize=(6.4, 4.8*1.15))
+#axg1 = figglob.add_subplot(3,1,1)
+axg2 = figglob.add_subplot(2,1,1)
+axg3 = figglob.add_subplot(2,1,2, sharex=axg2)
+
+#mpbl = axg1.pcolormesh(1e2*obsim.z_slices, 1e3*xg, -(1./qe)*obsim.rho_cut.T)
+#plt.colorbar(mpbl,orientation='horizontal')
+#axg1.plot(1e2*obsim.z_slices, 1e3*obsim.x_slices, 'k', lw=2)
+axg2.plot(1e2*obsim.z_slices, 1e3*x_test, lw=3., color='k')
+lines_pic = axg3.plot(1e2*obsim.z_slices, 1e6*obsim.dpx_slices,
+        lw=3., color='k', linestyle='-',
+        label='PIC')
+
+line_list = []
+for ifig, n_terms_to_be_kept in enumerate(n_terms_list):
 
     # Build matrix
     respmat = rm.ResponseMatrix(
@@ -74,11 +98,13 @@ for n_terms_to_be_kept in n_terms_list:
             n_terms_to_be_kept=n_terms_to_be_kept,
             n_tail_cut=n_tail_cut)
 
-    # Get simulation data
-    obsim = mfm.myloadmat_to_obj(test_data_file)
-    x_test = obsim.x_slices
-    int_test = obsim.int_slices
-    x_test[np.isnan(x_test)] = 0.
+    # Recenter all slices
+    slices = bunch.get_slices(slicer)
+    for ii in range(slices.n_slices):
+        ix = slices.particle_indices_of_slice(ii)
+        if len(ix) > 0:
+            bunch.x[ix] -= np.mean(bunch.x[ix])
+            bunch.xp[ix] -= np.mean(bunch.xp[ix])
 
     # Distort bunch
     bunch.x = bunch.x + np.interp(bunch.z, respmat.z_resp, x_test)
@@ -100,49 +126,71 @@ for n_terms_to_be_kept in n_terms_list:
     a_coeff, x_reconstr = respmat.decompose_trace(x_test)
 
     # Plots
-    import matplotlib.pyplot as plt
-    import PyECLOUD.mystyle as ms
-    plt.close('all')
-    ms.mystyle(fontsz=14, traditional_look=False)
 
     z_resp = respmat.z_resp
 
     n_freq = (n_terms_to_be_kept-1)/2
 
-    fig2 = plt.figure(2, figsize=(6.4, 4.8*1.5))
+    fig2 = plt.figure(200 + ifig, figsize=(6.4, 4.8*1.5))
     ax2 = fig2.add_subplot(3,1,2)
-    ax2.plot(z_resp, 1e3*x_test, label='Test trace')
-    ax2.plot(z_resp, 1e3*x_reconstr, label=f'Reconstructed (n={n_freq})')
+    ax2.plot(1e2*z_resp, 1e3*x_test, label='Test trace')
+    ax2.plot(1e2*z_resp, 1e3*x_reconstr, label=f'Reconstructed (n={n_freq})')
     ax2.set_ylim(1e3*np.nanmax(np.abs(x_test))*np.array([-1, 1]))
     ax2.set_ylabel('x [mm]')
     ax2.legend(prop={'size':12}, loc='lower right', ncol=2)
 
     ax3 = fig2.add_subplot(3,1,3, sharex=ax2)
-    ax3.plot(z_resp, 1e6*obsim.dpx_slices, label='Simulation')
-    ax3.plot(slices_test.z_centers, 1e6*slices_test.mean_xp, label=f'Harm. response (n={n_freq})')
+    ax3.plot(1e2*z_resp, 1e6*obsim.dpx_slices, label='Simulation')
+    ax3.plot(1e2*slices_test.z_centers, 1e6*slices_test.mean_xp, label=f'Harm. response (n={n_freq})')
     ax3.set_ylim(1e6*np.nanmax(np.abs(obsim.dpx_slices))*np.array([-1.5, 1.1]))
     ax3.set_ylabel('Dpx [urad]')
-    ax3.set_xlabel('z [m]')
+    ax3.set_xlabel('z [cm]')
     ax3.legend(prop={'size':12}, loc='lower right', ncol=2)
 
     for aa in [ax2, ax3]:
         aa.grid(linestyle=':', alpha=.9)
 
-    xg = obsim.xg
-    yg = obsim.yg
     i_yzero = np.argmin(np.abs(yg))
 
     ax21 = fig2.add_subplot(3,1,1, sharex=ax2)
 
-    ax21.pcolormesh(obsim.z_slices, 1e3*xg, obsim.rho_cut.T)
-    ax21.plot(obsim.z_slices, 1e3*obsim.x_slices, 'k', lw=2)
+    ax21.pcolormesh(1e2*obsim.z_slices, 1e3*xg, obsim.rho_cut.T)
+    ax21.plot(1e2*obsim.z_slices, 1e3*obsim.x_slices, 'k', lw=2)
     ax21.set_ylim(-2.5, 2.5)
     ax21.set_ylabel('x [mm]')
     ax21.set_title('Electron density', fontsize=14)
+    ax21.set_xlim(-30, 30)
 
     fig2.subplots_adjust(hspace=.22, left=.18,
             bottom=0.09, top=.93)
 
     fig2.savefig(test_data_file.split('.mat')[0] + f'_n{n_terms_to_be_kept:04d}.png', dpi=200)
 
+    kwargs = {}
+    if len(n_terms_list)==3:
+        kwargs['color'] = plt.cm.Reds([.3, .5, .8][ifig])
+    axg2.plot(1e2*slices_test.z_centers, 1e3*x_reconstr,
+            label=f'n={n_terms_to_be_kept}', **kwargs, lw=2)
+    ln = axg3.plot(1e2*slices_test.z_centers, 1e6*slices_test.mean_xp,
+            label=f'n={n_terms_to_be_kept}', **kwargs, lw=2)
+    line_list += ln
+
+#axg1.set_ylim(-2.5, 2.5)
+#axg1.set_ylabel('x [mm]')
+#axg1.xaxis.set_tick_params(labelbottom=False)
+
+axg2.set_ylim(-.3, .3)
+axg2.set_xlim(-30, 30)
+#bbox_to_anchor : ?,v-pos,width?
+axg3.legend(handles=lines_pic+line_list,
+        bbox_to_anchor=(0., -.4, 1, 0), loc='upper left',
+           ncol=4, mode="expand", borderaxespad=0., frameon=False)
+axg3.set_xlabel('z [cm]')
+axg2.set_ylabel('x [mm]')
+axg3.set_ylabel(r'$\Delta$p$_x$ [$\mu$rad]')
+axg3.ticklabel_format(style='sci', scilimits=(0,0),axis='y')
+axg2.ticklabel_format(style='sci', scilimits=(0,0),axis='y')
+axg2.xaxis.set_tick_params(labelbottom=False)
+#legpic = plt.legend(handles=lines_pic)
+figglob.subplots_adjust(bottom=.2)
 plt.show()
