14ac7931a593a011ef292ef0b9a07fd0e5f9806f
giadarol <giovanni.iadarola@cern.ch>
2020-05-09 19:51:47 +0200

    Status

--- 007_analysis_sim_scans/001d_comparare.py
+++ 007_analysis_sim_scans/001d_comparare.py
@@ -87,6 +87,8 @@ figharm_list = []
 figintra_list = []
 for ii, ll in enumerate(dict_plot.keys()):
     oo = mfm.myloadmat_to_obj(dict_plot[ll]['fname'])
+    if flag_mode_unstab:
+        insta_thresh = dict_plot[ll]['insta_thresh']
     tilt_lines = dict_plot[ll]['tilt_lines']
     scale_x = dict_plot[ll]['scale_x']
     kwargs = {}
@@ -144,8 +146,79 @@ for ii, ll in enumerate(dict_plot.keys()):
     figharm.subplots_adjust(right=.83)
     figharm_list.append(figharm)
 
-ax1.legend(bbox_to_anchor=(1, 1),  loc='upper left', fontsize='small')
-ax1.grid(True, linestyle=':')
+    # Plot data from intrabunch motion
+    all_freqs = np.concatenate(
+            (oo.freqs_1mode_re_list[mask_strength],
+             oo.freqs_1mode_im_list[mask_strength]), axis=1)
+    all_aps = np.concatenate(
+            (oo.ap_1mode_re_list[mask_strength],
+             oo.ap_1mode_im_list[mask_strength]), axis=1)
+    # Renorm to each colunms
+    for jjj, sss in enumerate(strength_list):
+        all_aps[jjj, :] /= np.mean(all_aps[jjj, all_aps[jjj, :]>0])
+    maxsizeintra = np.max(np.array(all_aps))
+    figintra = plt.figure(200+ii)
+    axintra = figintra.add_subplot(111, sharex=axshare, sharey=axshare)
+    str_mat_intra = np.dot(np.atleast_2d(np.ones(all_freqs.shape[1])).T,
+            np.atleast_2d(np.array(strength_list)))
+    scale_marker = 1.5
+    axintra.scatter(x=str_mat_intra.flatten(),
+            y=(np.abs(np.array(all_freqs)).T.flatten()-q_frac)/Qs,
+            s=np.clip(np.array(all_aps).T.flatten()/maxsizeintra*scale_marker,
+                0.0, scale_marker),
+            #c=np.clip(np.array(all_aps).T.flatten()/maxsizeintra, 0.3, 0.4),
+            cmap=cm.Blues, norm=Normalize(vmin=0, vmax=0.5),
+            color='C0')
+
+    if flag_mode_0:
+        # Plot mode zero
+        mask_plot_mode_0 = np.array(stre_mode_0) < insta_thresh
+        axintra.plot(np.array(stre_mode_0)[mask_plot_mode_0],
+                (np.array(freq_mode_0)[mask_plot_mode_0]-q_frac)/Qs, '.k')
+    if flag_mode_unstab:
+        # Plot unstable freq
+        freq_instab, ap_instab, stre_instab = extract_independent_lines(
+            strength_list, all_freqs, all_aps, 1e-3,
+            np.ones_like(strength_list, dtype=np.int))
+        mask_instab = np.array(stre_instab) > insta_thresh
+        axintra.plot(np.array(stre_instab)[mask_instab],
+                (np.array(freq_instab)[mask_instab]-q_frac)/Qs, '.',
+                color='C3')
+
+    axintra.set_yticks(np.arange(l_min+1, l_max-0.2))
+    axintra.grid(axis='y', linestyle='--')
+    axintra.set_xlabel('e-cloud strength')
+    axintra.set_ylabel(r'(Q - Q$_0$)/Q$_s$')
+    figintra.suptitle(ll)
+    figintra.subplots_adjust(bottom=.12, right=.85)
+    figintra_list.append(figintra)
+
+    # min_dist = 3e-3
+    # n_indep_list = np.zeros_like(strength_list, dtype=np.int) + 2
+    # n_indep_list[oo.n_sample_list<1000] = 1
+    # all_freq_indep, all_aps_indep, all_stre_indep = extract_independent_lines(
+    #     strength_list, all_freqs, all_aps, min_dist, n_indep_list)
+    # min_dist = 3e-3
+    # n_indep_list = np.zeros_like(strength_list, dtype=np.int) + 2
+    # n_indep_list[oo.n_sample_list<1000] = 1
+    # all_freq_indep, all_aps_indep, all_stre_indep = extract_independent_lines(
+    #     strength_list, all_freqs, all_aps, min_dist, n_indep_list)
+
+    # indep_normalized = (np.array(all_freq_indep)-q_frac)/Qs
+    # mask_keep = np.abs(indep_normalized)<2
+    # axintra.plot(np.array(all_stre_indep)[mask_keep],
+    #             indep_normalized[mask_keep], '.', color='C03')
+
+    # for i_sb in [-1, 1.1]:
+    #     freq_sb, ap_sb, stre_sb = extract_independent_lines(
+    #         strength_list, np.abs(np.array(freq_list)), np.array(ap_list),
+    #         min_dist=3e-3, n_indep_list=np.zeros_like(strength_list, dtype=np.int)+1,
+    #         allowed_range=(q_frac+(i_sb*Qs-0.1*Qs), q_frac + (i_sb + 1.)*Qs))
+    #     sb_normalized = (np.array(freq_sb)-q_frac)/Qs
+    #     axintra.plot(np.array(stre_sb), sb_normalized, '.', color='C01')
+
+ax1.legend(loc='upper left', fontsize='medium', frameon=False)
+#ax1.grid(True, linestyle=':')
 ax1.set_xlim(min_strength, max_strength)
 ax1.set_ylim(tau_min, tau_max)
 ax1.set_xlabel('e-cloud strength')
